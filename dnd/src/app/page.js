77dccd99c7ffe900c94426432e64152fcd1c36ca
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2023-08-24 15:44:58 +0545

    feat: Initialized Project

---
+++ dnd/src/app/page.js
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='text-primary w-full h-[100vh] text-2xl font-semibold flex justify-center items-center'>Get started 🚀🚀</div>
+  )
+}
