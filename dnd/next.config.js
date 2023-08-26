eee1cc2998a2f00820762901a42d3ee31b0bee75
Diwash Lamichhane <imdiwash.mrprogrammer@gmail.com>
2023-08-26 18:31:18 +0545

    feat: Integrate docs into the site@

--- dnd/next.config.js
+++ dnd/next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig ={
+    webpack: (config, { isServer }) => {
+      // Add a rule to handle PDF files
+      config.module.rules.push({
+        test: /\.(pdf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'static/pdf', // Change this to your desired output path
+              publicPath: '/_next/static/pdf', // Change this to your public path
+            },
+          },
+        ],
+      });
+  
+      // Your other configurations...
+  
+      return config;
+    },
+  };
+  
+  
+  
 
 module.exports = nextConfig
